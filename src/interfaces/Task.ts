04c46cb47ffdb744acf5dd4e3d38391ad193c3c0
Shlomi Nugarker <shlomin1231@gmail.com>
2024-10-14 15:09:02 +0300

    interface Task

--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -1,5 +1,8 @@
 export interface Task {
+  id?: string;
   task: string;
   assignee: string;
-  priority: string;
+  priority: Priority;
 }
+
+export type Priority = "High" | "Medium" | "Low" | "All";
