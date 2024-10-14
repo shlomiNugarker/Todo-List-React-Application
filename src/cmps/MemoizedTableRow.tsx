57d14728e67ef6e988b5c0feb4fdadf36f7dbde2
Shlomi Nugarker <shlomin1231@gmail.com>
2024-10-14 18:11:32 +0300

    style hover

--- src/cmps/MemoizedTableRow.tsx
+++ src/cmps/MemoizedTableRow.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const MemoizedTableRow: React.FC<Props> = React.memo(({ row }) => {
   return (
-    <tr className="flex flex-col sm:table-row">
+    <tr className="flex flex-col sm:table-row hover:bg-gray-100 dark:hover:bg-gray-700 ">
       {row.getVisibleCells().map((cell) => (
         <td
           key={cell.id}
