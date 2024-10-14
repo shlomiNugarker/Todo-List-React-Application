5b591c51a8f36313c5347d89564aa27dc79ea5e1
Shlomi Nugarker <shlomin1231@gmail.com>
2024-10-14 15:44:27 +0300

    useCallback, useMemo

--- src/cmps/TodoListTable.tsx
+++ src/cmps/TodoListTable.tsx
@@ -6,7 +6,9 @@ import {
   flexRender,
   SortingState,
 } from "@tanstack/react-table";
+import { useMemo, useCallback } from "react";
 import { Task } from "../interfaces/Task";
+import MemoizedTableRow from "./MemoizedTableRow";
 
 interface Props {
   tasks: Task[];
@@ -23,74 +25,83 @@ const TodoListTable = ({
   sorting,
   setSorting,
 }: Props) => {
-  const handleEditClick = (task: Task) => {
-    onClickEdit(task);
-  };
+  const handleEditClick = useCallback(
+    (task: Task) => {
+      onClickEdit(task);
+    },
+    [onClickEdit]
+  );
 
-  const handleDeleteClick = (task: Task) => {
-    deleteTask(task);
-  };
+  const handleDeleteClick = useCallback(
+    (task: Task) => {
+      deleteTask(task);
+    },
+    [deleteTask]
+  );
 
-  const columns: ColumnDef<Task>[] = [
-    {
-      accessorKey: "task",
-      header: "Task",
-    },
-    {
-      accessorKey: "assignee",
-      header: "Assignee",
-    },
-    {
-      accessorKey: "priority",
-      header: "Priority",
-    },
-    {
-      accessorKey: "edit",
-      header: "",
-      cell: ({ row }) => (
-        <button
-          className="text-blue-500 hover:text-blue-700"
-          onClick={() => handleEditClick(row.original)}
-          aria-label="Edit Task"
-        >
-          <svg
-            className="text-xl"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
+  const columns: ColumnDef<Task>[] = useMemo(
+    () => [
+      {
+        accessorKey: "task",
+        header: "Task",
+      },
+      {
+        accessorKey: "assignee",
+        header: "Assignee",
+      },
+      {
+        accessorKey: "priority",
+        header: "Priority",
+      },
+      {
+        accessorKey: "edit",
+        header: "",
+        cell: ({ row }) => (
+          <button
+            className="text-blue-500 hover:text-blue-700"
+            onClick={() => handleEditClick(row.original)}
+            aria-label="Edit Task"
           >
-            <path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
-          </svg>
-        </button>
-      ),
-    },
-    {
-      accessorKey: "delete",
-      header: "",
-      cell: ({ row }) => (
-        <button
-          aria-label="Delete Task"
-          className="text-red-500 hover:text-red-700"
-          onClick={() => handleDeleteClick(row.original)}
-        >
-          <svg
-            className="text-xl"
-            viewBox="0 0 1024 1024"
-            fill="currentColor"
-            height="1em"
-            width="1em"
+            <svg
+              className="text-xl"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+            >
+              <path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+            </svg>
+          </button>
+        ),
+      },
+      {
+        accessorKey: "delete",
+        header: "",
+        cell: ({ row }) => (
+          <button
+            aria-label="Delete Task"
+            className="text-red-500 hover:text-red-700"
+            onClick={() => handleDeleteClick(row.original)}
           >
-            <path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z" />
-          </svg>
-        </button>
-      ),
-    },
-  ];
+            <svg
+              className="text-xl"
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1em"
+              width="1em"
+            >
+              <path d="M360 184h-8c4.4 0 8-3.6 8-8v8h304v-8c0 4.4 3.6 8 8 8h-8v72h72v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80h72v-72zm504 72H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zM731.3 840H292.7l-24.2-512h487l-24.2 512z" />
+            </svg>
+          </button>
+        ),
+      },
+    ],
+    [handleEditClick, handleDeleteClick]
+  );
 
   const table = useReactTable({
     data: tasks,
@@ -109,12 +120,9 @@ const TodoListTable = ({
     "w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400";
   const thClassNames =
     "text-center px-3 py-2 sm:px-6 sm:py-3 bg-gray-50 dark:bg-gray-800 cursor-pointer ";
-  const tdClassNames =
-    "truncate text-center px-3 py-2  sm:px-6 sm:py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white text-wrap";
 
   return (
     <div className="relative overflow-x-auto shadow-md rounded-lg h-[70vh] w-[90vw] ">
-      {/* Table Section */}
       {tasks.length === 0 ? (
         <div className="flex justify-center items-center h-[70vh]">
           <p>No tasks available</p>
@@ -145,21 +153,8 @@ const TodoListTable = ({
             ))}
           </thead>
           <tbody className="">
-            {table.getRowModel().rows.map((row, rowIndex) => (
-              <tr
-                key={row.id}
-                className={`flex flex-col sm:table-row border-b border-gray-200 dark:border-gray-700 ${
-                  rowIndex % 2 === 0
-                    ? "bg-gray-50 dark:bg-gray-800"
-                    : "bg-white dark:bg-gray-900"
-                }`}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className={tdClassNames}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
+            {table.getRowModel().rows.map((row) => (
+              <MemoizedTableRow key={row.id} row={row} />
             ))}
           </tbody>
         </table>
